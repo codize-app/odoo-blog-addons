a71ab1b39d96ac4a23905089bc28668b1ac5115d
Ignacio Buioli <ibuioli@gmail.com>
2022-07-25 12:16:49 -0300

    fix: err

--- website_blog_tts/models/models.py
+++ website_blog_tts/models/models.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError
 import pyttsx3
 import base64
 import re
+import os
 
 engine = pyttsx3.init();
 engine.setProperty('voice', 'spanish')
@@ -14,17 +15,21 @@ regex = re.compile(r'<[^>]+>')
 class BlogPost(models.Model):
     _inherit = 'blog.post'
 
-    def generate_tts(self):
+    def remove_tts(self):
         self.content_tts = False
+
+    def generate_tts(self):
+        self.remove_tts()
         content = regex.sub('', self.content)
-        if len(content > 6500):
+        if len(content) > 6500:
             content = content[:6500]
         engine.save_to_file(content, '/tmp/speech.mp3')
         engine.runAndWait()
         file = open('/tmp/speech.mp3', 'rb')
         data = file.read()
         file.close()
+        os.unlink('/tmp/speech.mp3')
         encoded = base64.b64encode(data)
         self.content_tts = encoded
 
-    content_tts = fields.Binary('TTS', attachment=True)
+    content_tts = fields.Binary('TTS')

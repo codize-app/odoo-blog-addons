dd19156db47405096af1866da88ab0caf65b1dd1
Ignacio Buioli <ibuioli@gmail.com>
2022-08-10 15:10:04 -0300

    fix: minor style fixes

--- website_blog_related_post/static/src/js/related_post.js
+++ website_blog_related_post/static/src/js/related_post.js
@@ -32,13 +32,13 @@ odoo.define('website_blog_related_post.related_post_js', function (require) {
             for(var i=0; i < posts.posts.length; i++){
               console.log(posts.posts[i]);
               var s = '<div class="col-12 col-md-4 media s_latest_posts_post" style="opacity:0.8;">\
-                                                 <div class="media-body ml-3 pb-2">\
+                                                 <div class="media-body ml-3 pb-2 w-100">\
                                                     <h5>\
                                                       <a href="/blog/' + posts.posts[i].blog_id + '/post/' + posts.posts[i].id + '">\
-                                                        <span class="text-truncate">' + posts.posts[i].name  + '<span>\
+                                                        <span class="text-truncate d-block">' + posts.posts[i].name  + '<span>\
                                                       </a>\
                                                     </h5>\
-                                                    <p>' + posts.posts[i].teaser + '</p>
+                                                    <p class="text-blog-related">' + posts.posts[i].teaser + '</p>
                                                     <a class="btn btn-sm btn-primary" href="/blog/' + posts.posts[i].blog_id + '/post/' + posts.posts[i].id + '">Seguir leyendo</a>\
                                                   </div>\
                                               </div>';

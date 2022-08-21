4fe01a1d15ab1739c5427c5fb7f337ed75c4622c
Ignacio Buioli <ibuioli@gmail.com>
2022-08-21 16:55:46 -0300

    feat: add website_blog_tags

---
+++ website_blog_tags/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Blog Tags",
+
+    'summary': """
+        Tags view for Odoo Blog""",
+
+    'description': """
+        Add Tags Visual in Odoo Blog
+    """,
+
+    'author': "Codize",
+    'website': "https://www.codize.ar",
+
+    'category': 'Website',
+    'version': '0.1',
+
+    'depends': ['base', 'website', 'website_blog'],
+
+    'data': [
+        'views/views.xml',
+    ],
+
+    'demo': [
+    ],
+}
